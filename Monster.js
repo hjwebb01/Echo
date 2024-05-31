b8704b3336d2c1ef7730165af19954992ffe24bb
ewbrees <81719511+ewbrees@users.noreply.github.com>
2024-05-31 12:55:03 -0500

    monsters can be given a chance to teleport while in range of player

--- Monster.js
+++ Monster.js
@@ -1,5 +1,5 @@
 export class Monster {
-    constructor(x, y, width, height, isMoving, moveSpeed, player, visibility) {
+    constructor(x, y, width, height, isMoving, moveSpeed, player, visibility, chanceTeleport) {
         this.originalX = x;
         this.originalY = y;
         this.x = x;
@@ -12,23 +12,53 @@ export class Monster {
         this.player = player;
         this.visibility = visibility;
         this.isSighted = false;  // Play a sound for initial spotting
+        this.chanceTeleport = chanceTeleport;
+        this.teleportTimer = 0;
+        this.teleportInterval = 2000;
     }
 
-    update(endpointActive) {
-        // Only allow movement if textsShown is false
+    update(deltaTime, endpointActive) {
         if (this.isMovingInitially && endpointActive) {
             this.isMoving = true;
         }
 
         if (this.isMoving) {
+            this.teleportTimer += deltaTime;
+            if (this.teleportTimer >= this.teleportInterval) {
+                this.teleportTimer = 0;
+                this.checkAndTeleport();
+            }
             this.moveTowardPlayer();
         }
     }
+
 
     resetPosition() {
         this.x = this.originalX;
         this.y = this.originalY;
         this.isMoving = false; // Stop moving when resetting position
+    }
+
+    checkAndTeleport() {
+        const distance = this.calculateDistanceToPlayer();
+        if (distance < 300 && Math.random() < this.chanceTeleport) {
+            this.teleport();
+        }
+    }
+
+    teleport() {
+        let newX, newY, distance;
+        do {
+            newX = Math.random() * this.player.canvas.width;
+            newY = Math.random() * this.player.canvas.height;
+            const dx = this.player.x - newX;
+            const dy = this.player.y - newY;
+            distance = Math.sqrt(dx * dx + dy * dy);
+        } while (distance < 300 || distance > this.player.canvas.width); // Ensure teleportation is outside 300 units but within the canvas
+
+        this.x = newX;
+        this.y = newY;
+        this.isSighted = false;
     }
 
     moveTowardPlayer() {
@@ -59,6 +89,12 @@ export class Monster {
             this.y < other.y + other.height &&
             this.y + this.height > other.y
         );
+    }
+
+    calculateDistanceToPlayer() {
+        const dx = this.player.x - this.x;
+        const dy = this.player.y - this.y;
+        return Math.sqrt(dx * dx + dy * dy);
     }
 
     draw(ctx) {
